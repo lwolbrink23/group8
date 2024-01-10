d61d371e538ca623955f238ee217264bb4ad1e0b
Quynh Vo <quynh.vo3314@gmail.com>
2024-01-10 23:10:04 +0000

    Jan 10, 2024, 6:10 PM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header'
 import Home from './Pages/Home'
 
 function App() {
   return (
     <div className="App">
+    <Header/>
      <Home />
     </div>
   );
