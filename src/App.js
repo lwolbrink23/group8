a324da4d7b7dcb8c41d8fed6de068cc8e2df80d9
callievaughn <122556798+callievaughn@users.noreply.github.com>
2024-01-10 18:35:34 -0500

    added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './Styles/main.css'
 import Header from './Components/Header'
 import Home from './Pages/Home'
 import Footer from './Components/Footer'
+import OurSuites from './Pages/OurSuites';
 
 function App() {
   return (
     <div className="App">
     <Header/>
      <Home />
+     <OurSuites></OurSuites>
      <Footer />
     </div>
   );
