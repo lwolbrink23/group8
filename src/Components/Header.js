d07921459160600acf9fe75481ecb327f2493f5a
Quynh Vo <quynh.vo3314@gmail.com>
2024-01-10 23:08:08 +0000

    Jan 10, 2024, 6:08 PM

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,42 @@
 function Header() {
     return (
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+<header>
+      {/* nav bar */}
+      <div class="nav">
+        {/* <!--logo and navigation links container--> */}
+        <div class="logo-and-nav">
+            {/* <!--logo--> */}
+            <img src="assets/logo/TSS Circle logo Transparent.png" alt="Logo" class="logo" />
+            {/* <!--hamburger menu--> */}
+            <ul>
+            <li><a href="default.asp">Home</a></li>
+            <li class="dropdown">
+                <a href="#" class="about-link">About</a>
+                <ul class="subpages">
+                    <li><a href="our-story.html">Our Story</a></li>
+                    <li><a href="our-suites.html">Our Suites</a></li>
+                    <li><a href="our-services.html">Our Services</a></li>
+                    <li><a href="photo-gallery.html">Photo Gallery</a></li>
+                </ul>
+            </li>
+          <li><a href="linkhere">Blog</a></li>
+          <li><a href="linkhere">FAQ's</a></li>
+          <li><a href="linkhere">Shop</a></li>
+          <li><a href="linkhere">Contact Us</a></li>
+          <li><a href="linkhere">Account</a></li>
+          <li>
+            <button
+              type="button"
+              onclick="alert('Button clicked!')"
+              class="purp-button"
+            >
+              Book Now
+            </button>
+          </li>
+        </ul>
+      </div>
+      </div>
+    </header>
     );
   }
   
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import logo from '../logo.svg';
 import '../App.css';
+import Header from '../Components/Header'
 
 
 function Home() {
     return (
       <div className="App">
+        <Header/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
