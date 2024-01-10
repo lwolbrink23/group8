043cefe5739e89dcdff86d586f8410585ab57cba
Lillybliss <lillyallgood@knights.ucf.edu>
2024-01-10 18:58:41 -0500

    cleaned up css and files

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,43 +1,69 @@
 function Header() {
-    return (
-<header>
+  return (
+    <header>
       {/* nav bar */}
-      <div class="nav">
+      <div className="nav">
         {/* <!--logo and navigation links container--> */}
-        <div class="logo-and-nav">
-            {/* <!--logo--> */}
-            <img src="assets/logo/TSS Circle logo Transparent.png" alt="Logo" class="logo" />
-            {/* <!--hamburger menu--> */}
-            <ul>
-            <li><a href="default.asp">Home</a></li>
-            <li class="dropdown">
-                <a href="#" class="about-link">About</a>
-                <ul class="subpages">
-                    <li><a href="our-story.html">Our Story</a></li>
-                    <li><a href="our-suites.html">Our Suites</a></li>
-                    <li><a href="our-services.html">Our Services</a></li>
-                    <li><a href="photo-gallery.html">Photo Gallery</a></li>
-                </ul>
+        <div className="logo-and-nav">
+          {/* <!--logo--> */}
+          <img
+            src="assets/logo/TSS Circle logo Transparent.png"
+            alt="Logo"
+            class="logo"
+          />
+          {/* <!--hamburger menu--> */}
+          <ul>
+            <li>
+              <a href="default.asp">Home</a>
             </li>
-          <li><a href="linkhere">Blog</a></li>
-          <li><a href="linkhere">FAQ's</a></li>
-          <li><a href="linkhere">Shop</a></li>
-          <li><a href="linkhere">Contact Us</a></li>
-          <li><a href="linkhere">Account</a></li>
-          <li>
-            <button
-              type="button"
-              onclick="alert('Button clicked!')"
-              class="purp-button"
-            >
-              Book Now
-            </button>
-          </li>
-        </ul>
-      </div>
+            <li className="dropdown">
+              <a href="#" className="about-link">
+                About
+              </a>
+              <ul class="subpages">
+                <li>
+                  <a href="our-story.html">Our Story</a>
+                </li>
+                <li>
+                  <a href="our-suites.html">Our Suites</a>
+                </li>
+                <li>
+                  <a href="our-services.html">Our Services</a>
+                </li>
+                <li>
+                  <a href="photo-gallery.html">Photo Gallery</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="linkhere">Blog</a>
+            </li>
+            <li>
+              <a href="linkhere">FAQ's</a>
+            </li>
+            <li>
+              <a href="linkhere">Shop</a>
+            </li>
+            <li>
+              <a href="linkhere">Contact Us</a>
+            </li>
+            <li>
+              <a href="linkhere">Account</a>
+            </li>
+            <li>
+              <button
+                type="button"
+                onclick="alert('Button clicked!')"
+                className="purp-button"
+              >
+                Book Now
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
-    );
-  }
-  
-  export default Header;
+  );
+}
+
+export default Header;
