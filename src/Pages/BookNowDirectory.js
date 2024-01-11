1619f633c97d7899c5c2051cd23ff22dc716c755
lwolbrink23 <lindsey.wolbrink@outlook.com>
2024-01-11 17:24:05 -0500

    updating book now

--- src/Pages/BookNowDirectory.js
+++ src/Pages/BookNowDirectory.js
@@ -1,13 +1,23 @@
 import "../App.css";
 import "../Styles/booknow.css"
+import SimplyChicHair from '../assets/images/SimplyChicHair.png';
 
 function BookNowDirectory() {
     return (
         <div>
             <h1 className="center">Directory</h1>
             <div className="dropdown dropdown-content">
-                <div className="suites">
-                    <img />
+                <div className="suites" id="box1">
+                    <div className="top">
+                        <img src={SimplyChicHair} alt="SimplyChicHair" />
+                        <div className="description">
+                            <h3>Simply Chic Hair</h3>
+                            <p>Suite #1 <br />(321) 123-3211<br />Specialties: Hair cuts, color and styling.</p>
+                            <div className="buttons">
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
