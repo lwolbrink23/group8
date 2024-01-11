891bd2051dd76c8e813ff10e9a8e63c0cac11ba9
lwolbrink23 <lindsey.wolbrink@outlook.com>
2024-01-10 21:47:33 -0500

    header and footer to page

--- src/Pages/OurServices.js
+++ src/Pages/OurServices.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import '../App.css';
 import Header from '../Components/Header';
+import Footer from '../Components/Footer';
 
 function OurServices() {
     const HairContent = () => (
@@ -212,6 +213,7 @@ function OurServices() {
             <NailsDropdown />
             <MakeupDropdown />
             <MassageDropdown />
+            <Footer />
         </div>
     );
 }
