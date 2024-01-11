dcc3d7ea02bf49ec428fbce08ba7854f34c6322c
lwolbrink23 <lindsey.wolbrink@outlook.com>
2024-01-10 21:08:15 -0500

    OurServices done for the most part

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import './Styles/main.css'
 import Header from './Components/Header'
 import Home from './Pages/Home'
+import OurServices from './Pages/Our-Services';
 
 function App() {
   return (
