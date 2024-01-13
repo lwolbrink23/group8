52a34bfc61162054b673d64a685e4a202a724b50
lwolbrink23 <lindsey.wolbrink@outlook.com>
2024-01-13 17:27:49 -0500

    footer complete

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,7 @@
 import "../Styles/footer.css"
+import Facebook from "../assets/icons/facebook.png"
+import Insta from "../assets/icons/instagram.png"
+import Tiktok from "../assets/icons/tiktok.png"
 
 function Footer() {
   return (
@@ -7,7 +10,7 @@ function Footer() {
         <div className="left">
           <div className="footer-section suite-spot">
             <h2>The Suite Spot</h2>
-            <p><strong>Phone:</strong> 223-133-2948<br /><strong>Address:</strong> 2343 Suite Spot Way, Orlando, FL 32804</p>
+            <p><strong>Phone:</strong><br />223-133-2948<br /><strong>Address:</strong><br />2343 Suite Spot Way, Orlando, FL 32804</p>
 
           </div>
           <div className="footer-section hours">
@@ -16,14 +19,14 @@ function Footer() {
           </div>
         </div>
         <div className="footer-section newsletter">
-          <h3>Join our Newsletter</h3>
+          <h2>Join our Newsletter</h2>
           <input type="email" placeholder="Enter your email" />
           <button>Subscribe</button>
           <div className="social-icons">
             {/* Replace # with your social media links */}
-            <a href="#"><img src="/path-to-facebook-icon.png" alt="Facebook" /></a>
-            <a href="#"><img src="/path-to-instagram-icon.png" alt="Instagram" /></a>
-            <a href="#"><img src="/path-to-tiktok-icon.png" alt="TikTok" /></a>
+            <a href="#"><img src={Facebook} alt="Facebook" /></a>
+            <a href="#"><img src={Insta} alt="Instagram" /></a>
+            <a href="#"><img src={Tiktok} alt="TikTok" /></a>
           </div>
         </div>
       </div>
