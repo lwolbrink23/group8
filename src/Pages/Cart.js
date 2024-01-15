06bdb64443fc11555865c691a854ff9196060ca1
Quynh Vo <quynh.vo3314@gmail.com>
2024-01-14 22:37:47 -0500

    more cart stuff

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,13 +1,27 @@
 import shopICON from "../assets/icons/icons8-shopping-cart-100.png";
+import BackButton from "../Components/BackButton";
+import tempPic from "../assets/images/shop/color-protection-kit.png";
+import "../Styles/cart.css";
 
 function Cart() {
   return (
-    <div id="Cart">
+    <div id="cart">
       {/* title */}
       <div className="title-container trans-white">
-        <h1>Checkout</h1>
+        <BackButton />
+        <h1>Cart</h1>
         <img src={shopICON} alt="shopping cart" id="cart-icon"></img>
       </div>
+      <main>
+        <p>6 items in your cart</p>
+        <div className="items-container">
+          <img src={tempPic} alt=""></img>
+          <p>item</p>
+        </div>
+        <p>subtotal (6 items)</p>
+        <p>$###</p>
+        <button>Proceed to Checkout</button>
+      </main>
     </div>
   );
 }
