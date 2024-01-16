9402e994d78f6c5031b76f65d4a767f6cc6b6938
Quynh Vo <quynh.vo3314@gmail.com>
2024-01-16 18:39:23 -0500

    cart progress

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -13,14 +13,24 @@ function Cart() {
         <img src={shopICON} alt="shopping cart" id="cart-icon"></img>
       </div>
       <main>
-        <p>6 items in your cart</p>
-        <div className="items-container">
-          <img src={tempPic} alt=""></img>
-          <p>item</p>
+        <div>
+          <p>6 items in your cart</p>
+          <div className="items-container">
+            <div className="cart-item">
+              <img src={tempPic} alt=""></img>
+              <div>
+                <p>
+                  item name <br></br> instock
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-        <p>subtotal (6 items)</p>
-        <p>$###</p>
-        <button>Proceed to Checkout</button>
+        <div className="subtotal">
+          <p>subtotal (6 items)</p>
+          <p>$###</p>
+          <button>Proceed to Checkout</button>
+        </div>
       </main>
     </div>
   );
