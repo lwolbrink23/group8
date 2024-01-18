c5422030c9b8c372fd968bb8b9ef07004d9e9abf
BriaLatt <112596743+BriaLatt@users.noreply.github.com>
2024-01-18 00:37:28 -0500

    fixing scroll

--- src/Pages/OurStory.js
+++ src/Pages/OurStory.js
@@ -6,15 +6,6 @@ import scissors from "../assets/icons/scissors-whitebg.png";
 import diamond from "../assets/icons/diamond-whitebg.png";
 import shootingStar from "../assets/icons/star-whitebg.png";
 
-function ScrollToTop() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-}
 
 function OurStory() {
   return (
