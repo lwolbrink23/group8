72346aac5d1a55c3ea47f20534383fc840f9b118
Lillybliss <122473380+Lillybliss@users.noreply.github.com>
2024-01-19 13:08:38 -0500

    Add files via upload

---
+++ src/Components/MainCarousel.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "../Styles/maincarousel.css";
+
+const MainCarousel = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
+  };
+
+  return (
+    <div className="photo-carousel">
+      <button onClick={prevSlide}>&lt;</button>
+      <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
+      <button onClick={nextSlide}>&gt;</button>
+    </div>
+  );
+};
+
+export default MainCarousel;
