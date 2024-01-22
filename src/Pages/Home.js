1bc587cd18c23d925fcc639be3985f4724751219
Lillybliss <lillyallgood@ucf.edu>
2024-01-22 18:40:12 -0500

    trying to format images on home with divs

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,6 +7,8 @@ import promoHair from "../assets/images/promo_hair.png";
 import promoSpa from "../assets/images/promo_spa.png";
 import promoWella from "../assets/images/promo_wella.png";
 import webpromoBottle from "../assets/images/home banner bottle.png";
+import brownHair from "../assets/images/brownhair.jpg";
+import spaProducts from "../assets/images/spaproducts.png";
 
 function Home() {
   const mobilecarouselImages = [
@@ -55,60 +57,61 @@ function Home() {
       </div>
 
       {/* <!-- "Why Choose Us?" Section --> */}
-      <div className="why-choose-us-section">
-        <h2>Why Choose Us?</h2>
-        {/* <!-- I added a ul tag around the li elements - Lindsey --> */}
-        <ul>
-          <li>
-            We're all about empowering you to be your own boss. Enjoy the
-            freedom of setting your own hours and taking control of your career.
-          </li>
-          <li>
-            Our platform is designed to support your professional growth. We
-            provide the tools and infrastructure you need to focus on growing
-            your client base and income.
-          </li>
-          <li>
-            Decorate your suite in your unique, creative way! Your space is a
-            canvas where you can showcase your style and personality, creating
-            an inviting atmosphere for your clients.
-          </li>
-        </ul>
-        <p>
-          Unlock your potential and join "The Suite Spot" community. Discover
-          how we can help you build your beauty business on your own terms.
-        </p>
+      <div className="wcu">
+        <div className="why-choose-us-section">
+          <h2>Why Choose Us?</h2>
+          {/* <!-- I added a ul tag around the li elements - Lindsey --> */}
+          <ul>
+            <li>
+              We're all about empowering you to be your own boss. Enjoy the
+              freedom of setting your own hours and taking control of your
+              career.
+            </li>
+            <li>
+              Our platform is designed to support your professional growth. We
+              provide the tools and infrastructure you need to focus on growing
+              your client base and income.
+            </li>
+            <li>
+              Decorate your suite in your unique, creative way! Your space is a
+              canvas where you can showcase your style and personality, creating
+              an inviting atmosphere for your clients.
+            </li>
+          </ul>
+          <p>
+            Unlock your potential and join "The Suite Spot" community. Discover
+            how we can help you build your beauty business on your own terms.
+          </p>
+        </div>
+        <img src={spaProducts} alt="Spa Products" className="webHomepic1" />
       </div>
-      <div className="webHomepic1">
-        <img src="assets/images/spaproducts.png" alt="Spa Products" />
-      </div>
-      <div className="webHomepic2">
-        <img src="assets/images/brownhair.png" alt="Hair" />
-      </div>
-      {/* <!-- "Our Providers" Section --> */}
-      <div className="our-providers-section">
-        <h2>Our Providers</h2>
-        <p>
-          Our location is the perfect hub for a wide range of beauty and
-          wellness services. Here are some of the dedicated professionals you'll
-          find here:
-        </p>
-        <ul>
-          <li>Hairdressers</li>
-          <li>Makeup Artists</li>
-          <li>Nail Artists</li>
-          <li>Massage Therapists</li>
-        </ul>
-        <p>
-          Our community of Lifestyle Professionals is dedicated to helping you
-          look and feel your best. Discover a wide range of services under one
-          roof.
-        </p>
-        <Link to="/ourservices">
-          <button type="button" className="viewServButton">
-            View Our Services
-          </button>
-        </Link>
+      <div className="op">
+        <img src={brownHair} alt="Hair" className="webHomepic2" />
+        {/* <!-- "Our Providers" Section --> */}
+        <div className="our-providers-section">
+          <h2>Our Providers</h2>
+          <p>
+            Our location is the perfect hub for a wide range of beauty and
+            wellness services. Here are some of the dedicated professionals
+            you'll find here:
+          </p>
+          <ul>
+            <li>Hairdressers</li>
+            <li>Makeup Artists</li>
+            <li>Nail Artists</li>
+            <li>Massage Therapists</li>
+          </ul>
+          <p>
+            Our community of Lifestyle Professionals is dedicated to helping you
+            look and feel your best. Discover a wide range of services under one
+            roof.
+          </p>
+          <Link to="/ourservices">
+            <button type="button" className="viewServButton">
+              View Our Services
+            </button>
+          </Link>
+        </div>
       </div>
 
       {/* <!-- Gallery --> */}
