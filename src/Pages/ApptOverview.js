dced77758826dcbbd6ced25fcacf2d1f6b924e98
lwolbrink23 <lindsey.wolbrink@outlook.com>
2024-01-22 18:30:40 -0500

    fixed

--- src/Pages/ApptOverview.js
+++ src/Pages/ApptOverview.js
@@ -66,8 +66,8 @@ function Overview() {
                         const parts = service.split('\n'); // Split the service string into parts
                         return (
                             <div key={index} id="booked-service">
-                                <p>{parts[0]}<br />{parts[1]} - {parts[2]}</p> {/* Service name */}
-                                <p></p> {/* Service duration and price */}
+                                <p>{parts[0]}<br />{parts[1]}</p> {/* Service name */}
+                                <p>{parts[2]}</p> {/* Service duration and price */}
                             </div>
                         );
                     })}
