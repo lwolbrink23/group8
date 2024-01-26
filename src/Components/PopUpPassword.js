2b6c1a0ede6007ea8842ce2429f37ffcb6638029
lwolbrink23 <lindsey.wolbrink@outlook.com>
2024-01-26 14:45:15 -0500

    styling popup

--- src/Components/PopUpPassword.js
+++ src/Components/PopUpPassword.js
@@ -33,8 +33,8 @@ const PopupPassword = ({ isOpen, closePopup }) => {
         <div className="popup-background">
             <div className="popup-container">
                 {/*add dynamic html in here*/}
-                <label>Enter Your Email</label>
-                <input type="email" value={email} onChange={handleEmailChange} />
+                <label>Enter Your Email <br /></label>
+                <input type="email" value={email} onChange={handleEmailChange} className="popup-input" />
                 <button onClick={handleSubmit} disabled={!email}>Submit</button>
                 <button onClick={closePopup}>Close</button>
             </div>
