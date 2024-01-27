3cede5b98252ec552e7938b1caaac0bc187ab16c
BriaLatt <112596743+BriaLatt@users.noreply.github.com>
2024-01-26 22:20:31 -0500

    added blogpost 3. Need to make fixes later.

---
+++ src/Pages/BlogPost-three.js
@@ -0,0 +1,67 @@
+import React from "react";
+import "../Styles/blogpost.css";
+import BackButton from "../Components/BackButton";
+import kneadRelax from "../assets/images/relax.jpeg";
+function BlogPost3() {
+  return (
+    <div className="post1">
+      {/* back arrow */}
+      <BackButton />
+      <div className="post-general-info">
+        <img
+          src={kneadRelax}
+          className="main-image"
+          alt="woman getting a massage"
+        />
+        <h4 className="category">Category: Makeup</h4>
+        <h4 className="author">Author: Jane Doe</h4>
+        <h2 className="art-title">Knead to Relax: The Power of Massage</h2>
+      </div>
+      <p className="articles-contents">
+       In a world that often emphasizes youth as the epitome of beauty,
+        it's time to redefine the standards and celebrate the unique beauty
+        found at every age. Makeup, a versatile form of self-expression, becomes
+        a powerful ally in highlighting and enhancing our individual charm,
+        regardless of the number of candles on our birthday cake. **Embracing
+        Youthful Glow:** As teenagers embark on their makeup journey, it's a
+        time of experimentation and self-discovery. The focus here is on
+        simplicity a light touch of foundation to enhance natural radiance, a
+        hint of blush for that youthful flush, and perhaps a touch of lip gloss.
+        It's not about masking, but rather enhancing the fresh and vibrant glow
+        of youth. **Expressing Bold Individuality:** Entering the twenties is a
+        period of self-expression and bold experimentation. Vibrant eyeshadows,
+        daring lip colors, and expressive eyeliner become tools for showcasing
+        personality. The canvas is at its prime, allowing for the exploration of
+        various looks and styles. It's a time to play with makeup and embrace
+        the freedom to express oneself through color and creativity.
+        **Cultivating Timeless Elegance:** As individuals transition into their
+        thirties, a focus on timeless elegance takes center stage. Makeup
+        becomes a tool for highlighting natural beauty. Defined eyebrows,
+        neutral eyeshadows, and classic red lips contribute to an effortlessly
+        sophisticated look. This age is about understanding the art of subtlety
+        and investing in quality products that enhance without overpowering.
+        **Celebrating Wisdom and Experience:** In the forties, makeup is a
+        celebration of wisdom and experience. Subtle, neutral tones and
+        hydrating foundations become key players in accentuating features. A
+        graceful approach to makeup, coupled with a robust skincare routine,
+        embraces the changes in skin with confidence and poise. It's a stage of
+        embracing oneself and celebrating the journey. **Glamorous Confidence at
+        Any Age:** Beyond the fifties, makeup becomes a tool for cultivating
+        glamorous confidence. Soft, neutral tones, nourishing skincare, and a
+        focus on self-care redefine beauty. This is a time to radiate
+        confidence, embracing the wisdom and grace that comes with age. Makeup
+        becomes an art of celebrating a life well-lived, reflecting the beauty
+        that transcends any societal standards. In conclusion, makeup is a
+        journey that evolves with age, reflecting the diverse stages of life.
+        It's not about conforming to societal ideals but about celebrating
+        individuality and embracing the beauty found at every age. So, whether
+        you're just starting to experiment with makeup or have decades of
+        experience, let it be a form of self-love and expression, embracing the
+        uniqueness that each age brings. After all, beauty is timeless, and it
+        knows no boundaries.
+      </p>
+    </div>
+  );
+}
+
+export default BlogPost3;
--- src/Pages/blog.js
+++ src/Pages/blog.js
@@ -1,15 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../App.css"
 import "../Styles/blog.css";
 import hairWashing from "../assets/images/hairwashing.jpg"
 import kneadRelax from "../assets/images/relax.jpeg"
+import allAges from "../assets/images/allages.jpeg"
 
 function ScrollToTop() {
   const location = useLocation();
 
   useEffect(() => {
-    if (location.pathname === "/blogpost") {
+    if (location.pathname === "blog/blogpost/:category/:index") {
       window.scrollTo(0, 0);
     }
   }, [location.pathname]);
@@ -22,7 +23,39 @@ function Blog() {
     window.scrollTo(0, 0);
   };
 
+   const [selectedCategory, setSelectedCategory] = useState("Filter by Topic"); // Default to show all posts
+
+  const handleFilter = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const posts = [
+    {
+      category: "hair",
+      image: hairWashing,
+      title: "Hair Care Secrets: Tips for Gorgeous Locks",
+      content: "In this article, we'll delve into effective tips that will leave you with a mane to envy.",
+    },
+    {
+      category: "massage",
+      image: kneadRelax,
+      title: "Knead to Relax: The Power of Massage",
+      content: "Discover the healing touch of massage and its incredible power to improve well-being and relaxation.",
+    },
+    {
+      category: "makeup",
+      image: allAges,
+      title: "Makeup for All Ages",
+      content: "Celebrate the beauty of every stage of life.",
+    }
+    // Add more posts with categories as needed
+  ];
+
+  const categories = [ "hair", "nails", "makeup", "massage"];
+
+
   return (
+    <div>
     <div>
       <ScrollToTop />
       <div className="main-blog-page">
@@ -45,33 +78,37 @@ function Blog() {
         <div class="overlay-box"></div>
       </div>
       <br />
+<div className="filter-dropdown">
+  <select
+    id="category"
+    onChange={(e) => handleFilter(e.target.value)}
+    value={selectedCategory}
+  >
+    <option value="Filter by Topic">Filter by Topic</option>
+    {categories.map((category) => (
+      <option key={category} value={category}>
+        {category.charAt(0).toUpperCase() + category.slice(1)}
+      </option>
+    ))}
+  </select>
+</div>
+      </div>
+      <br />
       <div className="posts-container">
-        <div className="hair-secrets">
-           {/* post1 image */}
-        <img src={hairWashing} alt="woman getting her hair washed by a hairstylist." />
-          <h3><b>Hair Care Secrets: Tips for Gorgeous Locks</b></h3>
-          <p>
-            In this article, we'll delve into effective tips that will leave you with a mane to envy.
-          </p>
-          <Link to="/blogpost" onClick={scrollToTop}>
-                  <button
-            type="button">
-            Read More
-          </button>
-            </Link>
-        </div>
-        <div className="knead-relax">
-          <img src={kneadRelax} alt="woman getting her hair washed by a hairstylist." />
-          <h3><b>Knead to Relax: The Power of Massage</b></h3>
-          <p>Discover the healing touch of massage and its 
-            incredible power to improve well-being and relaxation.</p>
-            <Link to="/blogpost" onClick={scrollToTop}>
-                  <button
-            type="button">
-            Read More
-          </button>
-            </Link>
-        </div>
+        {posts
+          .filter((post) => selectedCategory === "Filter by Topic" || post.category === selectedCategory)
+          .map((post, index) => (
+            <div key={index} className={`${post.category}`}>
+              <img src={post.image} alt={`${post.title} - ${post.category}`} />
+              <h3>
+                <b>{post.title}</b>
+              </h3>
+              <p>{post.content}</p>
+              <Link to="/blogpost" onClick={scrollToTop}>
+                <button type="button">Read More</button>
+              </Link>
+            </div>
+          ))}
       </div>
     </div>
   );
