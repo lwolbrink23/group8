a1df13afd93491bb916973d4be0ee66c86727045
Lillybliss <lillyallgood@ucf.edu>
2024-01-29 18:33:38 -0500

    form validation

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,19 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 import "../Styles/Login.css";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const [emailValue, setEmailValue] = useState("");
+  const [pwValue, setPwValue] = useState("");
+
+  const handleEmailInputChange = (event) => {
+    setEmailValue(event.target.value);
+  };
+
+  const handlePwInputChange = (event) => {
+    setPwValue(event.target.value);
+  };
+
+  const resetForm = () => {
+    setEmailValue("");
+    setPwValue("");
+  };
+
+  const handleLogin = () => {
+    resetForm();
+  };
+
+  const buttonStyle = {
+    color: emailValue && pwValue ? "black" : "#646464",
+  };
+
   return (
     <div>
       <h1>Log In</h1>
       <div className="contain">
         <form className="loginform">
           <label htmlFor="email">Email</label>
-          <input type="text" className="textarea" id="email" />
+          <input
+            type="text"
+            className="textarea"
+            id="email"
+            value={emailValue}
+            onChange={handleEmailInputChange}
+          />
           <br />
           <label htmlFor="password">Password</label>
-          <input type="text" className="textarea" id="password" />
+          <input
+            type="text"
+            className="textarea"
+            id="password"
+            value={pwValue}
+            onChange={handlePwInputChange}
+          />
           <br />
           <div className="checkbox-container">
             <input type="checkbox" className="check" id="check" />
@@ -25,9 +61,16 @@ function Login() {
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
+              textDecorationLine: "none",
             }}
           >
-            <button type="button" className="logbutton">
+            <button
+              type="button"
+              className="logbutton"
+              style={buttonStyle}
+              disabled={!emailValue || !pwValue}
+              onClick={handleLogin}
+            >
               LOG IN
             </button>
           </Link>
--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 import "../Styles/SignUp.css";
 import { Link } from "react-router-dom";
 
 function SignUp() {
+  const [firstNameValue, setFirstNameValue] = useState("");
+  const [lastNameValue, setLastNameValue] = useState("");
+  const [emailValue, setEmailValue] = useState("");
+  const [phoneValue, setPhoneValue] = useState("");
+  const [pwValue, setPwValue] = useState("");
+  const [confPwValue, setConfPwValue] = useState("");
+
+  const handleFirstNameInputChange = (event) => {
+    setFirstNameValue(event.target.value);
+  };
+
+  const handleLastNameInputChange = (event) => {
+    setLastNameValue(event.target.value);
+  };
+
+  const handleEmailInputChange = (event) => {
+    setEmailValue(event.target.value);
+  };
+
+  const handlePhoneInputChange = (event) => {
+    setPhoneValue(event.target.value);
+  };
+
+  const handlePwInputChange = (event) => {
+    setPwValue(event.target.value);
+  };
+
+  const handleConfPwInputChange = (event) => {
+    setConfPwValue(event.target.value);
+  };
+
+  const resetForm = () => {
+    setFirstNameValue("");
+    setLastNameValue("");
+    setEmailValue("");
+    setPwValue("");
+    setConfPwValue("");
+  };
+
+  const handleSignUp = () => {
+    resetForm();
+  };
+
+  const buttonStyle = {
+    color:
+      firstNameValue && lastNameValue && emailValue && pwValue && confPwValue
+        ? "black"
+        : "#646464",
+  };
   return (
     <div>
       <h1>Sign Up</h1>
@@ -12,31 +61,89 @@ function SignUp() {
           <div className="inline">
             <div>
               <label htmlFor="firstname">First Name</label>
-              <input type="text" className="section1" id="first" />
+              <input
+                type="text"
+                className="section1"
+                id="first"
+                value={firstNameValue}
+                onChange={handleFirstNameInputChange}
+              />
             </div>
             <div>
               <label htmlFor="lastname">Last Name</label>
-              <input type="text" className="section1" id="last" />
+              <input
+                type="text"
+                className="section1"
+                id="last"
+                value={lastNameValue}
+                onChange={handleLastNameInputChange}
+              />
             </div>
           </div>
           <div className="inline">
             <div>
               <label htmlFor="phonenumber">Phone Number</label>
-              <input type="text" className="section1" id="phone" />
+              <input
+                type="text"
+                className="section1"
+                id="phone"
+                value={phoneValue}
+                onChange={handlePhoneInputChange}
+              />
             </div>
             <div>
               <label htmlFor="email">Email Address</label>
-              <input type="text" className="section1" id="email" />
+              <input
+                type="text"
+                className="section1"
+                id="email"
+                value={emailValue}
+                onChange={handleEmailInputChange}
+              />
             </div>
           </div>
           <label htmlFor="password">Password</label>
-          <input type="text" className="textarea" id="password" />
+          <input
+            type="text"
+            className="textarea"
+            id="password"
+            value={pwValue}
+            onChange={handlePwInputChange}
+          />
           <br></br>
           <label htmlFor="confirm">Confirm Password</label>
-          <input type="text" className="textarea" id="confirm" />
+          <input
+            type="text"
+            className="textarea"
+            id="confirm"
+            value={confPwValue}
+            onChange={handleConfPwInputChange}
+          />
           <br></br>
-          <Link to="/Account">
-            <button type="button">Sign Up</button>
+          <Link
+            to="/Account"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              textDecorationLine: "none",
+            }}
+          >
+            <button
+              type="button"
+              style={buttonStyle}
+              disabled={
+                !firstNameValue ||
+                !lastNameValue ||
+                !emailValue ||
+                !phoneValue ||
+                !pwValue ||
+                !confPwValue
+              }
+              onClick={handleSignUp}
+            >
+              Sign Up
+            </button>
           </Link>
         </form>
       </div>
