a5b79193413d08ab0ac1ef04db4b2fc279908ad7
callievaughn <122556798+callievaughn@users.noreply.github.com>
2024-01-29 15:38:08 -0500

    finished appt details

--- src/Pages/ApptDetails.js
+++ src/Pages/ApptDetails.js
@@ -9,32 +9,50 @@ function ApptDetails() {
   return (
     <div>
       <div className="appt-heading">
-        <h1>Appointment Details</h1>
+        <h1 className="appt-title">Appointment Details</h1>
       </div>
       <div className="appt-form">
-        <div className="prof-image">
-          <img src={SimplyChicHair} alt="Simply Chic Hair" />
+        <div className="top-section">
+          <div className="prof-image">
+            <img src={SimplyChicHair} alt="Simply Chic Hair" />
+          </div>
+
+          <div className="middle-section">
+            <p className="bolded-title">Simply Chic Hair</p>
+            <div className="icon-move">
+              <img src={calendaricon} alt="Calendar Icon" className="calendar-icon" />
+              <div className="icon-with-text">
+                <p>Sep 03, 2023</p>
+                <p>2:00pm-4:00pm</p>
+              </div>
+            </div>
+  
+
+          <div className="status-container">
+            <p className="bolded-status">Status:</p>
+            <p className="complete">COMPLETE</p>
+          </div>
+          </div>
         </div>
-        <p>Simply Chic Hair</p>
-        <img src={calendaricon} alt="Calendar Icon" />
-        <p>Sep 03, 2023</p>
-        <p>2:00pm-4:00pm</p>
-        <p>Status:</p>
-        <p>COMPLETE</p>
-        <p>Time</p>
-        <p>2:00pm</p>
-        <p>Duration</p>
-        <p>2hrs</p>
-        <p>Services</p>
-        <p>Full Balayage</p>
-        <p>Price</p>
-        <p>$200</p>
-        <p>Staff</p>
-        <p>Jean C.</p>
-        <Link to="/Account">
-          <button type="button">View Profile</button>
-        </Link>
-      </div>
+
+        <div className="bottom-section">
+          <p className="bolded-time">Time</p>
+          <p>2:00pm</p>
+          <p className="bolded">Duration</p>
+          <p>2hrs</p>
+          <p className="bolded">Services</p>
+          <p>Full Balayage</p>
+          <p className="bolded">Price</p>
+          <p>$200</p>
+          <p className="bolded">Staff</p>
+          <p>Jean C.</p>
+          </div>
+          <Link to="/Account">
+            <button type="button" className="profile-">
+              Back to Profile
+            </button>
+          </Link>
+        </div>
     </div>
   );
 }
