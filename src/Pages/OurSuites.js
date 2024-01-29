f3e7a99d901e7b55072e39c5bc07cb7bd289e934
Lillybliss <lillyallgood@ucf.edu>
2024-01-29 17:02:24 -0500

    fixed our suites page formatting

--- src/Pages/OurSuites.js
+++ src/Pages/OurSuites.js
@@ -1,15 +1,12 @@
 // App.js or your desired component file
-import PopupContact from '../Components/PopUpContact';
-import React, { useState } from 'react';
-import '../App.css';
-import '../Styles/oursuites.css'
-import oursuitesimg1 from '../assets/images/oursuitesimg1.png';
-import oursuitesimg2 from '../assets/images/oursuitesimg2.png';
-
-
+import PopupContact from "../Components/PopUpContact";
+import React, { useState } from "react";
+import "../App.css";
+import "../Styles/oursuites.css";
+import oursuitesimg1 from "../assets/images/oursuitesimg1.png";
+import oursuitesimg2 from "../assets/images/oursuitesimg2.png";
 
 function OurSuites() {
-
   const [isContactOpen, setIsContactOpen] = useState(false);
 
   const openContact = () => setIsContactOpen(true);
@@ -19,7 +16,7 @@ function OurSuites() {
     <div>
       <div>
         <h1>Our Suites</h1>
-        <p className='p'>
+        <p className="topOurSuitesp">
           Step into our suites, and let the stress of the day melt away as you
           embark on a journey of beauty and well-being. Our suites are
           thoughtfully designed to provide a relaxing and comfortable
@@ -27,10 +24,10 @@ function OurSuites() {
           our priority.
         </p>
       </div>
-      <div className='centerit'>
+      <div className="centerit">
         <div className="blurb-container">
           <div className="blurbs">
-            <img src={oursuitesimg1} alt='Suite Pic 1' />
+            <img src={oursuitesimg1} alt="Suite Pic 1" />
             <h2>Quality Relaxation</h2>
             <p>
               From serene lighting to plush seating, we've thought of everything
@@ -38,34 +35,49 @@ function OurSuites() {
             </p>
           </div>
           <div className="blurbs">
-            <img src={oursuitesimg2} alt='Suite Pic 2' />
+            <img src={oursuitesimg2} alt="Suite Pic 2" />
             <h2>Impeccable Design</h2>
             <p>
               Each suite is meticulously designed to create a welcoming and
-              aesthetically pleasing space. You'll find comfort and sophistication
-              in every corner.
+              aesthetically pleasing space. You'll find comfort and
+              sophistication in every corner.
             </p>
           </div>
         </div>
       </div>
-      <div className='container'>
+      <div className="container">
         <div className="rent-section">
           <h1>Rent a Suite</h1>
-          <div className='pmain'>
+          <div className="pmain">
             <p>
-              Are you a Lifestyle Professional looking to lease a suite? Contact us
-              below, and a manager will get back to you.
+              Are you a Lifestyle Professional looking to lease a suite? Contact
+              us below, and a manager will get back to you.
             </p>
           </div>
           <form className="formarea">
-            <label htmlFor='name'></label>
-            <input type="text" className='fields' id="name" placeholder="Name" />
-            <label htmlFor='email'></label>
-            <input type="text" className='fields' id="email" placeholder="Email" />
-            <label htmlFor='subject'></label>
-            <input type="text" className='fields' id="subject" placeholder="Subject" />
-            <label htmlFor='message'></label>
-            <textarea className='fields' placeholder="Message"></textarea>
+            <label htmlFor="name"></label>
+            <input
+              type="text"
+              className="fields"
+              id="name"
+              placeholder="Name"
+            />
+            <label htmlFor="email"></label>
+            <input
+              type="text"
+              className="fields"
+              id="email"
+              placeholder="Email"
+            />
+            <label htmlFor="subject"></label>
+            <input
+              type="text"
+              className="fields"
+              id="subject"
+              placeholder="Subject"
+            />
+            <label htmlFor="message"></label>
+            <textarea className="fields" placeholder="Message"></textarea>
             <button type="button" onClick={openContact}>
               Send Message
             </button>
