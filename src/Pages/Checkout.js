82df1b0018e7a74fb3f4f0c2d544db415ebab382
Quynh Vo <quynh.vo3314@gmail.com>
2024-01-29 23:09:44 -0500

    form validation checkpoint

--- src/Pages/Checkout.js
+++ src/Pages/Checkout.js
@@ -76,8 +76,27 @@ function Checkout() {
       [propertyName]: value,
     }));
   };
+  const updatePersonalErr = (propertyName, value) => {
+    setPersonalErr((prevInfo) => ({
+      ...prevInfo,
+      [propertyName]: value,
+    }));
+  };
+  const updateAddressErr = (propertyName, value) => {
+    setAddressErr((prevInfo) => ({
+      ...prevInfo,
+      [propertyName]: value,
+    }));
+  };
+  const updatePaymentErr = (propertyName, value) => {
+    setPaymentErr((prevInfo) => ({
+      ...prevInfo,
+      [propertyName]: value,
+    }));
+  };
 
   const handleChange = (type, propertyName, value) => {
+    let errOccured = false;
     switch (type) {
       // --------personal info--------
       case "personal":
@@ -85,6 +104,10 @@ function Checkout() {
         switch (propertyName) {
           case "firstName":
             // make sure no numbers
+            if (/\d/.test(value)) {
+              updatePersonalErr(propertyName, "Names must not contain numbers");
+              errOccured = true;
+            }
             break;
           case "phone":
             // make sure only numbers
@@ -92,6 +115,7 @@ function Checkout() {
           default:
             break;
         }
+        !errOccured && updatePersonalErr(propertyName, "");
         break;
       // --------address info--------
       case "address":
@@ -250,21 +274,52 @@ function Checkout() {
             <p style={{ color: "red" }}>{personalErr.firstName}</p>
           )}
           <div>
-            <input type="text" placeholder="Last Name*" required />
+            <input
+              type="text"
+              placeholder="Last Name*"
+              onChange={(e) =>
+                handleChange("personal", "lastName", e.target.value)
+              }
+            />
           </div>
           {personalErr.lastName && (
             <p style={{ color: "red" }}>{personalErr.lastName}</p>
           )}
           <div>
-            <input type="text" placeholder="Phone*" required />
+            <input
+              type="text"
+              placeholder="Phone*"
+              onChange={(e) =>
+                handleChange("personal", "phone", e.target.value)
+              }
+            />
           </div>
           <div>
-            <input type="text" placeholder="Street Address*" required />
+            <input
+              type="text"
+              placeholder="Street Address*"
+              onChange={(e) =>
+                handleChange("address", "street", e.target.value)
+              }
+            />
           </div>
           <div className="ct-state">
-            <input type="text" placeholder="City*" required />
-            <input type="text" placeholder="State*" required />
-            <input type="text" placeholder="ZIP Code*" required />
+            <input
+              type="text"
+              placeholder="City*"
+              onChange={(e) => handleChange("address", "city", e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="State*"
+              onChange={(e) => handleChange("address", "state", e.target.value)}
+            />
+
+            <input
+              type="text"
+              placeholder="ZIP Code*"
+              onChange={(e) => handleChange("address", "zip", e.target.value)}
+            />
           </div>
         </div>
         <div id="payment-info" className="cardbox">
@@ -285,14 +340,32 @@ function Checkout() {
             </div>
           ))}
           <div>
-            <input type="text" placeholder="Card Number*" required />
+            <input
+              type="text"
+              placeholder="Card Number*"
+              onChange={(e) =>
+                handleChange("payment", "cardNum", e.target.value)
+              }
+            />
           </div>
           <div className="cc-info">
-            <input type="text" placeholder="mm*" required />
+            <input
+              type="text"
+              placeholder="mm*"
+              onChange={(e) => handleChange("payment", "mm", e.target.value)}
+            />
 
-            <input type="text" placeholder="yy*" required />
+            <input
+              type="text"
+              placeholder="yy*"
+              onChange={(e) => handleChange("payment", "yy", e.target.value)}
+            />
 
-            <input type="text" placeholder="CVC*" required />
+            <input
+              type="text"
+              placeholder="CVC*"
+              onChange={(e) => handleChange("payment", "cvc", e.target.value)}
+            />
           </div>
           <div>
             <input type="text" placeholder="Name on Card*" required />
