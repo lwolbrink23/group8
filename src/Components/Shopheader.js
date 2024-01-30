c67fb0872c8176cdac7bfbee437cc405e008ac5d
Quynh Vo <quynh.vo3314@gmail.com>
2024-01-30 18:01:02 -0500

    added cart qty

--- src/Components/Shopheader.js
+++ src/Components/Shopheader.js
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
 import shopICON from "../assets/icons/icons8-shopping-cart-100.png";
 import BackButton from "./BackButton";
+import cartData from "../data/cart.json";
 
+// backend: load cart data and count (?)
 function Shopheader(props) {
   return (
     <div className="title-container trans-white">
-      <BackButton />
+      {!props.disableBack && <BackButton />}
       <h1>{props.htitle}</h1>
       <Link to="/cart">
-        <img src={shopICON} alt="shopping cart" id="cart-icon"></img>
+        <div className="cart-icon">
+          <img src={shopICON} alt="shopping cart" id="cart-icon"></img>
+          {1 > 0 && <span className="cart-quantity">{cartData.length}</span>}
+        </div>
       </Link>
     </div>
   );
