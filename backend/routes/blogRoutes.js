33c94328be7ddbe38b3b7b67de7b045c1b722bfe
lwolbrink23 <lindsey.wolbrink@outlook.com>
2024-02-14 14:51:35 -0500

    currently workingon blogposts.js mapping from mongodb

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -10,5 +10,17 @@ const blogRoutes = (app, database) => {
             res.status(500).json({ error: "Internal Server Error" });
         }
     });
+
+    app.get("/blog/:category/:id", async (req, res) => {
+    try {
+      const collection = database.collection("Blogs");
+      const query = { id: req.params.id, category: req.params.category };
+      const result = await collection.findOne({ query });
+      res.send(JSON.stringify(result));
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
 };
 export default blogRoutes;
