b992876f8ca4761c8e2232494ffc8a0b569a66b0
Quynh Vo <quynh.vo3314@gmail.com>
2024-02-16 23:10:58 -0500

    fetch from mongodb with auto id success

--- backend/routes/shopRoutes.js
+++ backend/routes/shopRoutes.js
@@ -38,14 +38,15 @@ const shopRoutes = (app, database) => {
       });
     }
   });
+  // .find({ id: req.params.id })
   app.get("/order_placed/:id", async (req, res) => {
     try {
       const collection = database.collection("User_Orders");
       const result = await collection
         .find({ _id: new ObjectId(req.params.id) })
-        // .find({ id: req.params.id })
         .toArray();
       res.send(JSON.stringify(result));
+      console.log(JSON.stringify(result));
     } catch (error) {
       console.error("Error fetching data:", error);
       res.status(500).json({ error: "Internal Server Error" });
