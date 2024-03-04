e06e18070c3130a91fda527568ff87ef2120853b
Lillybliss <lillyallgood@ucf.edu>
2024-03-04 13:24:02 -0500

    cancel button styling

--- src/Components/PopUpCancel.js
+++ src/Components/PopUpCancel.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import '../Styles/Popup.css';
+import React from "react";
+import "../Styles/Popup.css";
 
 const PopUpCancel = ({ isOpen, closePopUp, cancelAppt }) => {
-    if (!isOpen) return null;
+  if (!isOpen) return null;
 
-    return (
-        <div className="popup-background">
-            <div className="popup-container">
-                <p>Are you sure you want to cancel?</p>
-                <button onClick={() => { closePopUp(); cancelAppt(); }}>Yes</button>
-                <button onClick={closePopUp}>No</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="popup-background">
+      <div className="popup-container">
+        <p>Are you sure you want to cancel?</p>
+        <button
+          onClick={() => {
+            closePopUp();
+            cancelAppt();
+          }}
+        >
+          Yes
+        </button>
+        <button onClick={closePopUp} className="cancelPopupBtn">
+          No
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PopUpCancel;
