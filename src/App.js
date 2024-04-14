2553a53d526e1b97db7603580b25278b6b9ccb24
Quynh Vo <quynh.vo3314@gmail.com>
2024-04-13 21:11:47 -0400

    debug stuff

--- src/App.js
+++ src/App.js
@@ -29,7 +29,10 @@ import Account from "./Pages/Account";
 import ApptDetails from "./Pages/ApptDetails";
 import ContactUs from "./Pages/ContactUs";
 import { Route, Routes } from "react-router-dom";
+
 export const BACKEND_ADDRESS = "https://the-suite-spot-backend.vercel.app";
+// export const BACKEND_ADDRESS = "http://localhost:3003";
+
 // this is only the home page elements
 function App() {
   return (
